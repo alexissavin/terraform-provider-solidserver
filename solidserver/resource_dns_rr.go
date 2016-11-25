d277ceed2d41033e71a16d41d677f21ef0df4185
alexissavin <alexis.savin@gmail.com>
2016-11-25 16:27:14 +0100

    Working DNS rr management

--- solidserver/resource_dns_rr.go
+++ solidserver/resource_dns_rr.go
@@ -2,9 +2,11 @@ package solidserver
 
 import (
   "github.com/hashicorp/terraform/helper/schema"
+  "encoding/json"
   "net/url"
-  //"fmt"
-  "log"
+  "strings"
+  "fmt"
+  //"log"
 )
 
 func resourcednsrr() *schema.Resource {
@@ -26,6 +28,7 @@ func resourcednsrr() *schema.Resource {
         ForceNew: true,
       },
       "type": &schema.Schema{
+        ValidateFunc: resourcednsrrvalidatetype,
         Type:     schema.TypeString,
         Required: true,
         ForceNew: true,
@@ -45,48 +48,114 @@ func resourcednsrr() *schema.Resource {
   }
 }
 
+func resourcednsrrvalidatetype(v interface{}, _ string) ([]string, []error) {
+  switch strings.ToUpper(v.(string)){
+    case "A":
+      return nil, nil
+    case "AAAA":
+      return nil, nil
+    case "CNAME":
+      return nil, nil
+    default:
+      return nil, []error{fmt.Errorf("Unsupported RR type.")}
+  }
+}
+
 func resourcednsrrCreate(d *schema.ResourceData, meta interface{}) error {
   s := meta.(*SOLIDserver)
-
-  //FIXME Create DNS entry with name as FQDN in the specified zone with proper type and value
-  //mandatory_addition_params": "(rr_name && rr_type && value1 && (dns_id || dns_name || hostaddr))"
 
   // Building parameters
   parameters := url.Values{}
   parameters.Add("dns_name", d.Get("dnsserver").(string))
   parameters.Add("rr_name", d.Get("name").(string))
-  parameters.Add("rr_type", d.Get("type").(string))
+  parameters.Add("rr_type", strings.ToUpper(d.Get("type").(string)))
   parameters.Add("value1", d.Get("value").(string))
+  parameters.Add("rr_ttl", d.Get("ttl").(string))
 
-  // Sending the request
-  s.Request("post", "rest/dns_rr_add", &parameters)
+  // Sending the creation request
+  //resp, body, errs := s.Request("post", "rest/dns_rr_add", &parameters)
+  http_resp, body, _ := s.Request("post", "rest/dns_rr_add", &parameters)
 
-  log.Printf("[DEBUG] SOLIDserver Client : %#v", s)
+  var buf [](map[string]interface{})
+  json.Unmarshal([]byte(body), &buf)
 
-  return nil
+  if (http_resp.StatusCode == 201 && buf[0]["errno"].(int) <= 1) {
+    d.SetId(buf[0]["ret_oid"].(string))
+    return nil
+  }
+
+  return fmt.Errorf("Unable to create the RR record %s", d.Get("name").(string))
 }
 
 func resourcednsrrUpdate(d *schema.ResourceData, meta interface{}) error {
-  //apiclient := meta.(*resty.Client)
+  s := meta.(*SOLIDserver)
 
-  //FIXME Update DNS entry's value based on its id
-  //mandatory_edition_params": "(rr_id || (rr_name && rr_type && value1 && (dns_id || dns_name || hostaddr)))
+  // Building parameters
+  parameters := url.Values{}
+  parameters.Add("rr_id", d.Id())
+  parameters.Add("dns_name", d.Get("dnsserver").(string))
+  parameters.Add("rr_name", d.Get("name").(string))
+  parameters.Add("rr_type", strings.ToUpper(d.Get("type").(string)))
+  parameters.Add("value1", d.Get("value").(string))
+  parameters.Add("rr_ttl", d.Get("ttl").(string))
 
-  return nil
+  // Sending the update request
+  http_resp, body, _ := s.Request("put", "rest/dns_rr_add", &parameters)
+
+  var buf [](map[string]interface{})
+  json.Unmarshal([]byte(body), &buf)
+
+  if (http_resp.StatusCode == 200 && buf[0]["errno"].(int) <= 1) {
+    d.SetId(buf[0]["ret_oid"].(string))
+    return nil
+  }
+
+  return fmt.Errorf("Unable to update the RR record %s", d.Get("name").(string))
 }
 
 func resourcednsrrDelete(d *schema.ResourceData, meta interface{}) error {
-  //apiclient := meta.(*resty.Client)
+  s := meta.(*SOLIDserver)
 
-  //FIXME Delete DNS entry based on its id
+  // Building parameters
+  parameters := url.Values{}
+  parameters.Add("rr_id", d.Id())
+
+  // Sending the deletion request
+  _, body, _ := s.Request("delete", "rest/dns_rr_delete", &parameters)
+
+  var buf [](map[string]interface{})
+  json.Unmarshal([]byte(body), &buf)
 
   return nil
 }
 
 func resourcednsrrRead(d *schema.ResourceData, meta interface{}) error {
-  //apiclient := meta.(*resty.Client)
+   s := meta.(*SOLIDserver)
 
-  //FIXME Update local information based on RR id
+  // Building parameters
+  parameters := url.Values{}
+  parameters.Add("rr_id", d.Id())
 
-  return nil
+  // Sending the deletion request
+  http_resp, body, _ := s.Request("get", "rest/dns_rr_info", &parameters)
+
+  if (http_resp.StatusCode == 204) {
+    d.SetId("")
+    return fmt.Errorf("Couldn't find RR record %s", d.Get("name").(string))
+  }
+
+  var buf [](map[string]interface{})
+  json.Unmarshal([]byte(body), &buf)
+
+  if (http_resp.StatusCode == 200 && buf[0]["errno"].(int) <= 1) {
+    d.Set("dnsserver", buf[0]["dns_name"].(string))
+    d.Set("name", buf[0]["rr_full_name"].(string))
+    d.Set("type", buf[0]["rr_type"].(string))
+    d.Set("value", buf[0]["value1"].(string))
+    d.Set("ttl", buf[0]["ttl"].(string))
+
+    return nil
+  }
+
+  return fmt.Errorf("Unable to read RR record %s", d.Get("name").(string))
 }
